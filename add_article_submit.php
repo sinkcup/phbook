11b50a055479be4d0045c1625aa7ff2a370908fd
sinkcup <sinkcup@163.com>
2014-06-05 22:53:18 +0800

    feature: php save and show article

---
+++ add_article_submit.php
@@ -0,0 +1,13 @@
+<?php
+$input = $_POST;
+$file = './articles.json';
+$data = array();
+if (file_exists($file)) {
+    $tmp = file_get_contents($file); //读取文件，变成字符串
+    if(!empty($tmp)) {
+        $data = json_decode($tmp, true); //json解码成array
+    }
+}
+$data[] = $input;
+file_put_contents($file, json_encode($data)); //把字符串保存到文件
+echo '保存成功';
---
+++ index.php
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-Hans" lang="zh-Hans">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title>在线阅读</title>
+    </head>
+    <body>
+        <h1>在线阅读</h1>
+        <div><a href="./add_article.html">写文章</a></div>
+        <?php
+        $file = './articles.json';
+        if (file_exists($file)) {
+            $tmp = file_get_contents($file);
+            if (!empty($tmp)) {
+                echo '<ul>';
+                $data = json_decode($tmp, true);
+                foreach($data as $k=>$v) {
+                    $id = $k + 1;
+                    echo '<li><a href="./get_article.php?id=' . $id . '">' . $v['title'] . '</a>';
+                    echo '<p>' . mb_substr($v['content'], 0, 100, 'UTF-8') . '</p></li>';
+                }
+                echo '</ul>';
+            }
+        }
+        ?>
+    </body>
+</html>
