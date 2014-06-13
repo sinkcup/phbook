aa6e2fb5a5c506bbb9da9640d9270ac7e10ac287
sinkcup <sinkcup@163.com>
2014-06-13 18:35:47 +0800

    feature: separate view and logic

--- add_article_submit.php
+++ add_article_submit.php
@@ -4,10 +4,15 @@ $file = './articles.json';
 $data = array();
 if (file_exists($file)) {
     $tmp = file_get_contents($file); //读取文件，变成字符串
-    if(!empty($tmp)) {
+    if (!empty($tmp)) {
         $data = json_decode($tmp, true); //json解码成array
     }
 }
 $data[] = $input;
 file_put_contents($file, json_encode($data)); //把字符串保存到文件
-echo '保存成功';
+
+$d = array(); //d 是 data的意思，后续会用到
+$d['notice'] = array(
+    'msg' => '保存成功',
+);
+require_once __DIR__ . '/notice.html';
--- get_article.php
+++ get_article.php
@@ -1,28 +1,15 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-Hans" lang="zh-Hans">
-    <?php
-    $input = $_GET;
-    $id = $input['id'];
-    $file = './articles.json';
-    if (file_exists($file)) {
-        $tmp = file_get_contents($file);
-        if (!empty($tmp)) {
-            $articles = json_decode($tmp, true);
-            $article = $articles[$id-1];
-        }
+<?php
+$input = $_GET;
+$id = $input['id'];
+$file = './articles.json';
+if (file_exists($file)) {
+    $tmp = file_get_contents($file);
+    if (!empty($tmp)) {
+        $articles = json_decode($tmp, true);
+        $article = $articles[$id-1];
     }
-    ?>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <title><?=$article['title']?></title>
-    </head>
-    <body>
-        <h1><?=$article['title']?></h1>
-        <div>作者：<?=$article['author']?></div>
-        <div><?=nl2br($article['content'])?>
-        </div>
-    </body>
-</html>
+}
+
+$d = array();
+$d['article'] = $article;
+require_once __DIR__ . '/get_article.html';
