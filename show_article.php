082cb62587d6806b813c3e94ec548bebaf31f72f
sinkcup <sinkcup@live.it>
2017-02-20 16:50:38 +0800

    add home link

--- show_article.php
+++ show_article.php
@@ -1,8 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-Hans" lang="zh-Hans">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cmn-Hans" lang="cmn-Hans">
     <?php
     $input = $_GET;
     $id = $input['id'];
@@ -16,10 +14,11 @@
     }
     ?>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <title><?=$article['title']?></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title><?=$article['title']?> - 在线阅读网</title>
     </head>
     <body>
+        <div><a href="./">回首页</a></div>
         <h1><?=$article['title']?></h1>
         <div>作者：<?=$article['author']?></div>
         <div><?=nl2br($article['content'])?>
